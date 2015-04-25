7d97b70d1831c2d16aa46299049e827d267c33ed
Lakshmi <vemuri.lakshmi@ymail.com>
2015-04-25 11:10:47 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function returns:
+##              1. set the matrix
+##              2. get the matrix
+##              3. set the inverse
+##              4. get the inverse
+## this list is the input to the function cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## x is a square invertible matrix
+        inv = NULL
+        set = function(y) {
+                # `<<-` is used to assign a value to an object in an environment 
+                # different from the current environment. 
+                x <<- y
+                inv <<- NULL
+        }
+        get = function() x
+        setinv = function(inverse) 
+                inv <<- inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve returns inverse of the original matrix (input to makeCacheMatrix())
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv = x$getinv()
+        # if the inverse has already been calculated
+        # get it from the cache and skips the computation.
+        if (!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # otherwise, calculates the inverse 
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        # sets the value of the inverse in the cache via the setinv function.
+        x$setinv(inv)
+        
+        return(inv)
 }
+
+#Test makeCacheMatrix and cacheSolve
+testinvmatrix = function(mat){
+        temp = makeCacheMatrix(mat)
+        cacheSolve(temp)      
+}
+
+# Create input variable for the makeCacheMatrix using random numbers
+r = rnorm(36)
+mat = matrix(r, nrow=6, ncol=6)
+
+# Execute the test using the above variable
+testinvmatrix(mat)
+
+
